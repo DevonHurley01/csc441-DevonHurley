af988c198ff9b80da0770a6063bcb3708994d58f
Devon Hurley <devonh624@gmail.com>
2026-09-11 15:24:22 -0400

    Week 3 Wed Lab

--- csc441-Kotlin1/src/week3wed/Lab.kt
+++ csc441-Kotlin1/src/week3wed/Lab.kt
@@ -1,2 +1,29 @@
 package week3wed
 
+fun main() {
+
+    val name: String = "Devon"
+    val major = "Computer Science"
+    val Favoritefood = "Steak"
+
+    println("---Step 1---")
+    println(name)
+    println(major)
+    println(Favoritefood)
+
+    println("---Step 2---")
+    var numCourses = 10
+    println(numCourses)
+    println("courses now ${numCourses + 1}")
+
+    println("---Step 3---")
+    println("${name} is taking CSC-441")
+    println("What is your name?")
+    val yourName = readlnOrNull()
+    println("Nice to meet you ${yourName}")
+    println("${yourName} has ${yourName?.length} characters")
+    val student = true
+    val credits = 3.0
+    println("${name} is a student = ${student}")
+    println("CSC-441 is ${credits} credits in total")
+}
